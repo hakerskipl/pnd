1ab6e48b27cf462b28ce0992aeedd436ee83a7b3
Mateusz Gostanski <hakerski.pl@gmail.com>
2013-03-22 14:51:30 +0100

    Fix in views.py - using RequestContext within views.

--- pnd/views.py
+++ pnd/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render_to_response
 from pnd.models import *
 
 def index(request):
-	return render_to_response('index.html')
+	return render_to_response('index.html', context_instance=RequestContext(request))
 
 def results(request):
-	return render_to_response('wyniki.html')
+	return render_to_response('wyniki.html', context_instance=RequestContext(request))
 
 def detail(request):
-	return render_to_response('detail.html')
+	return render_to_response('detail.html', context_instance=RequestContext(request))
