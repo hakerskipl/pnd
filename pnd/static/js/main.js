deba0572dd4e8c3e3e2e9327a639da97b956d609
Mateusz Gostanski <hakerski.pl@gmail.com>
2013-04-01 15:08:38 +0200

    Cleaning.

--- pnd/static/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$('div.radio-click').on('click', function() {
-		$('div.radio-click').removeClass('checked').find('input[type="radio"]').attr('checked', false);
-		$(this).addClass('checked').find('input[type="radio"]').attr('checked', true);
-	});
-});
