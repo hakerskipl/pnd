404b6f57bf9503cff8d6b0c2cdfd5503944aa9e4
Mateusz Gostanski <hakerski.pl@gmail.com>
2013-04-08 21:22:14 +0200

    fucking przecinek

--- pnd/urls.py
+++ pnd/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     url(r'^index/$', index, name='index'),
     url(r'^results/$', results, name='results'),
     url(r'^detail/(?P<id>\d+)/$', detail, name='detail'),
-    url(r'^feellucky/$', feelLucky, name='feelLucky')
+    url(r'^feellucky/$', feelLucky, name='feelLucky'),
 
     url(r'^szukaj/$', search, name='search'),
     url(r'^typeahead/(?P<search>\w+)/$', typeahead, name='typeahead'),
