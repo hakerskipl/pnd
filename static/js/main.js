293bc8627c9e42092c0239d9f244121183d579f1
Mateusz Gostanski <hakerski.pl@gmail.com>
2013-04-09 18:49:07 +0200

    Responsive now works fine, optimized for smartphones. Background optimized for every of certain resolutions.

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,2 @@
 $(document).ready(function() {
-	$('div.radio-click').on('click', function() {
-		$('div.radio-click').removeClass('checked').find('input[type="radio"]').attr('checked', false);
-		$(this).addClass('checked').find('input[type="radio"]').attr('checked', true);
-	});
-
 });
