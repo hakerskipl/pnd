470d6ae9b0f70a9117b57e09bb85b000dcb454aa
Mateusz Gostanski <hakerski.pl@gmail.com>
2013-04-14 18:43:45 +0200

    Added Grappeli admin UI

--- pomysl/settings.py
+++ pomysl/settings.py
@@ -145,7 +145,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
+    'grappelli'
+,    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
--- pomysl/urls.py
+++ pomysl/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^grappelli/', include('grappelli.urls')),
 )
 
 # handler404 = 'pnd.views.Error404'
