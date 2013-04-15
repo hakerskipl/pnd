5fbe7c8e172e6d72f204c913d797b660dd70ee5e
Mateusz Gostanski <hakerski.pl@gmail.com>
2013-04-15 15:29:31 +0200

    Updates in settings.py. Optimized django-compressor

--- pomysl/settings.py
+++ pomysl/settings.py
@@ -56,13 +56,13 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = PROJECT_DIR + '../media/'
+MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/www/pomysl/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -121,6 +121,18 @@ COMPRESS_PRECOMPILERS = (
 COMPRESS_ROOT = PROJECT_DIR + '/../static/'
 
 COMPRESS_OFFLINE = True
+
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.template.TemplateFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+
+COMPRESSOR_JS_FILTERS = [
+    'compressor.filters.template.TemplateFilter',
+    'compressor.filters.jsmin.JSMinFilter',
+]
+
+COMPRESS_OUTPUT_DIR = 'min'
 
 CACHES = {
     'default': {
