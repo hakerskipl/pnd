97ae74ce6eb6c737e764f3df813c41a6a8e2fd25
Mateusz Gostanski <hakerski.pl@gmail.com>
2013-04-16 12:28:19 +0200

    Optimizations at images, and django-compressor

--- pomysl/settings.py
+++ pomysl/settings.py
@@ -104,6 +104,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
