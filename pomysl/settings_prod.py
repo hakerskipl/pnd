61c166e0209511dadd10ceaba3ab9ebfb2d0835f
Mateusz Gostanski <hakerski.pl@gmail.com>
2013-04-20 15:49:11 +0200

    Working on production settings.

--- pomysl/settings_prod.py
+++ pomysl/settings_prod.py
@@ -25,7 +25,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -115,7 +115,7 @@ ROOT_URLCONF = 'pomysl.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'pomysl.wsgi.application'
 
-COMPRESS_ENABLED = False
+COMPRESS_ENABLED = True
 
 COMPRESS_PRECOMPILERS = (
     ('text/less', 'lessc {infile} {outfile}'),
