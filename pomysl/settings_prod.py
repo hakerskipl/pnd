1b71c7b4f9cf9fc43a292b28ea5d7c48b2346eee
Mateusz Gostański <hakerski.pl@gmail.com>
2013-04-23 00:18:59 +0200

    Customize Grappeli Admin UI

--- pomysl/settings_prod.py
+++ pomysl/settings_prod.py
@@ -153,8 +153,6 @@ CACHES = {
     }
 }
 
-# GRAPPELLI_ADMIN_TITLE = u'Pomysł na dziś'
-
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
