2f2852efdb96349f692d8a56bbbd494afa3ff0ab
Mateusz Gostanski <hakerski.pl@gmail.com>
2013-04-23 09:57:30 +0200

    Fixes in settings.

--- pomysl/settings/common.py
+++ pomysl/settings/common.py
@@ -73,7 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_DIR, '../static'), 
+    os.path.join(PROJECT_DIR, '../../static'), 
 )
 
 # List of finder classes that know how to find static files in
@@ -120,7 +120,7 @@ COMPRESS_PRECOMPILERS = (
     ('text/less', 'lessc {infile} {outfile}'),
 )
 
-COMPRESS_ROOT = PROJECT_DIR + '/../static/'
+COMPRESS_ROOT = PROJECT_DIR + '/../../static/'
 
 COMPRESS_OFFLINE = False
 
@@ -158,7 +158,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_DIR, '../templates'),
+    os.path.join(PROJECT_DIR, '../../templates'),
 )
 
 INSTALLED_APPS = (
