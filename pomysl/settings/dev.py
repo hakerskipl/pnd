5218023f13fee1cb050cf615b2c5c2da599f4b65
Mateusz Gostański <hakerski.pl@gmail.com>
2013-04-27 00:02:56 +0200

    Fix in new-details, and added dev settings.

---
+++ pomysl/settings/dev.py
@@ -0,0 +1,18 @@
+ #-*- coding: utf-8 -*-
+from common import *
+
+MIDDLEWARE_CLASSES = (
+    # 'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
+    # 'django.middleware.cache.FetchFromCacheMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+DEBUG = TEMPLATE_DEBUG = True
